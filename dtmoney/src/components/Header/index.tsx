565909585ac708dedce4d054600e8e1c811e7f23
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-29 12:31:31 -0300

    react-modal

--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
+import Modal from 'react-modal'
 import { Container } from './styles'
-import {Content} from './styles'
+import { Content } from './styles'
 
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void
+}
 
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+    
 
-export function Header(){
-    return(
+    return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type = "button">Nova transação</button>
+                <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
+                
             </Content>
         </Container>
     )
