8e3db0054e813818f8d6b474e7d0a66c956b8b65
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-30 18:16:54 -0300

    Finalizando inserção

--- dtmoney/src/TransactionsContext.tsx
+++ dtmoney/src/TransactionsContext.tsx
@@ -16,7 +16,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData{
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
 
@@ -32,8 +32,16 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, [])
 
-    function createTransaction(transaction: TransactionInput) {
-        api.post('/transactions', transaction)
+    async function createTransaction(transactionInput: TransactionInput) {
+       const response = await api.post('/transactions', {
+           ...transactionInput,
+           createdAt: new Date(),
+       })
+       const {transaction}  = response.data
+       setTransactions([
+           ...transactions,
+           transaction
+       ])
     }
 
     return (
